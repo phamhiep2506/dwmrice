7ddd56fa7e1f0b04f9d1173271a52fcbcb2ec0e7
phiepdev <phamhiep25062k@gmail.com>
2024-10-10 03:36:31 +0700

    update nvim lualine

--- nvim/lua/plugins/gruvbox-material.lua
+++ nvim/lua/plugins/gruvbox-material.lua
@@ -1,13 +1,13 @@
 return {
   "sainnhe/gruvbox-material",
   config = function()
-    vim.g.gruvbox_material_foreground = "original"
-    vim.g.gruvbox_material_background = "hard"
-    vim.g.gruvbox_material_visual = "reverse"
-    vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
-    vim.g.gruvbox_material_enable_bold = true
-    vim.g.gruvbox_material_enable_italic = true
-    vim.g.gruvbox_material_better_performance = true
+    local g = vim.g
+    g.gruvbox_material_foreground = "original"
+    g.gruvbox_material_background = "hard"
+    g.gruvbox_material_diagnostic_virtual_text = "colored"
+    g.gruvbox_material_enable_bold = true
+    g.gruvbox_material_enable_italic = true
+    g.gruvbox_material_better_performance = true
     vim.cmd.colorscheme("gruvbox-material")
   end,
 }
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,32 +3,14 @@ return {
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     local fn = vim.fn
-    local lualine = require("lualine")
 
     local colors = {
       bg = "#282828",
       fg = "#ebdbb2",
       red = "#fb4934",
       green = "#b8bb26",
-      yellow = "#fabd2f",
       blue = "#83a598",
-      purple = "#d3869b",
-      aqua = "#8ec07c",
-      orange = "#fe8019",
-    }
-
-    local conditions = {
-      buffer_not_empty = function()
-        return fn.empty(fn.expand("%:t")) ~= 1
-      end,
-      hide_in_width = function()
-        return fn.winwidth(0) > 80
-      end,
-      check_git_workspace = function()
-        local filepath = fn.expand("%:p:h")
-        local gitdir = fn.finddir(".git", filepath .. ";")
-        return gitdir and #gitdir > 0 and #gitdir < #filepath
-      end,
+      yellow = "#fabd2f",
     }
 
     local config = {
@@ -44,18 +26,18 @@ return {
       sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_y = {},
-        lualine_z = {},
         lualine_c = {},
         lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
       },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_y = {},
-        lualine_z = {},
         lualine_c = {},
         lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
       },
     }
 
@@ -72,50 +54,32 @@ return {
         return "▊"
       end,
       color = { fg = colors.blue },
-      padding = { left = 0, right = 1 },
+      padding = { left = 0, right = 0 },
     })
 
     ins_left({
       function()
-        return " "
+        return ""
       end,
       color = function()
         local mode_color = {
           n = colors.red,
           i = colors.green,
           v = colors.blue,
-          [""] = colors.blue,
-          V = colors.blue,
-          c = colors.red,
-          no = colors.red,
-          s = colors.orange,
-          S = colors.orange,
-          [""] = colors.orange,
-          ic = colors.yellow,
-          R = colors.purple,
-          Rv = colors.purple,
-          cv = colors.red,
-          ce = colors.red,
-          r = colors.aqua,
-          rm = colors.aqua,
-          ["r?"] = colors.aqua,
-          ["!"] = colors.red,
-          t = colors.red,
+          V = colors.yellow,
         }
         return { fg = mode_color[fn.mode()] }
       end,
-      padding = { left = 0 },
+      padding = { left = 1, right = 1 },
     })
 
     ins_left({
       "filename",
-      cond = conditions.buffer_not_empty,
       color = { gui = "bold" },
     })
 
     ins_left({
       "filesize",
-      cond = conditions.buffer_not_empty,
     })
 
     ins_left({
@@ -127,12 +91,7 @@ return {
     ins_left({
       "diff",
       symbols = { added = " ", modified = " ", removed = " " },
-      diff_color = {
-        added = { fg = colors.green },
-        modified = { fg = colors.orange },
-        removed = { fg = colors.red },
-      },
-      cond = conditions.hide_in_width,
+      colored = true,
     })
 
     ins_right({
@@ -163,24 +122,14 @@ return {
         hint = "󰌶 ",
         info = " ",
       },
-      diagnostics_color = {
-        error = { fg = colors.red },
-        warn = { fg = colors.yellow },
-        hint = { fg = colors.green },
-        info = { fg = colors.aqua },
-      },
+      colored = true,
     })
 
-    ins_right({
-      "o:encoding",
-      cond = conditions.hide_in_width,
-      color = { gui = "bold" },
-    })
+    ins_right({ "o:encoding" })
 
     ins_right({
       "fileformat",
       icons_enabled = true,
-      color = { gui = "bold" },
     })
 
     ins_right({
@@ -195,9 +144,9 @@ return {
         return "▊"
       end,
       color = { fg = colors.blue },
-      padding = { right = 0 },
+      padding = { left = 0, right = 0 },
     })
 
-    lualine.setup(config)
+    require("lualine").setup(config)
   end,
 }
