59f5ff5b247408aded601c4b6e34b2601dd8616d
phiepdev <phamhiep25062k@gmail.com>
2024-10-12 10:41:05 +0700

    update icon nvim telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,6 +2,13 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = "nvim-lua/plenary.nvim",
   config = function()
+    require("telescope").setup({
+      defaults = {
+        prompt_prefix = " ",
+        selection_caret = " ",
+      },
+    })
+
     local map = vim.keymap.set
     map("n", "<leader>ff", "<CMD>Telescope find_files<CR>")
     map("n", "<leader>fg", "<CMD>Telescope live_grep<CR>")
