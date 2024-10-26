c69929065d96af35c0edc0ffc8a01ae3d1bea251
phiepdev <phamhiep25062k@gmail.com>
2024-10-26 21:09:31 +0700

    update code

---
+++ code/extensions.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+code --install-extension editorconfig.editorconfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension jdinhlife.gruvbox
+code --install-extension kylinideteam.cmake-intellisence
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension ms-vscode.cpptools-themes
+code --install-extension pkief.material-icon-theme
+code --install-extension s-nlf-fh.glassit
+code --install-extension twxs.cmake
+code --install-extension vscodevim.vim
