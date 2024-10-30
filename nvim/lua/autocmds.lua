e0d8bd9aa9b80ccba725f379625a019a7bf079a0
phamhiep2506 <phamhiep25062k@gmail.com>
2024-10-30 16:52:32 +0700

    update nvim

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -7,5 +7,6 @@ autocmd("BufEnter", {
     hl(0, "Normal", { bg = "NONE" })
     hl(0, "NormalNC", { bg = "NONE" })
     hl(0, "CursorLine", { fg = "NONE", bg = "NONE" })
+    hl(0, "CursorLineNR", { fg = "#fabd2f", bg = "NONE", bold = true })
   end,
 })
