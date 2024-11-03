dcb71bb1d147b14ee7cd869a73435d994e4af574
phamhiep2506 <phamhiep25062k@gmail.com>
2024-11-03 19:22:09 +0700

    update code extension

--- code/extensions.sh
+++ code/extensions.sh
@@ -4,11 +4,16 @@ code --install-extension christian-kohler.path-intellisense
 code --install-extension csharpier.csharpier-vscode
 code --install-extension editorconfig.editorconfig
 code --install-extension esbenp.prettier-vscode
+code --install-extension humao.rest-client
 code --install-extension jdinhlife.gruvbox
 code --install-extension kylinideteam.cmake-intellisence
 code --install-extension ms-dotnettools.csdevkit
 code --install-extension ms-dotnettools.csharp
 code --install-extension ms-dotnettools.vscode-dotnet-runtime
+code --install-extension ms-python.black-formatter
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
 code --install-extension ms-vscode.cmake-tools
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.cpptools-extension-pack
