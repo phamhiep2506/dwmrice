deb5cfe93ce257fc2f4c403fc99d12b43da4d4a6
phamhiep2506 <phamhiep25062k@gmail.com>
2024-11-18 22:16:51 +0700

    add config vscode

---
+++ code/extensions.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+code --install-extension christian-kohler.path-intellisense
+code --install-extension csharpier.csharpier-vscode
+code --install-extension editorconfig.editorconfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension humao.rest-client
+code --install-extension jdinhlife.gruvbox
+code --install-extension kylinideteam.cmake-intellisence
+code --install-extension ms-dotnettools.csdevkit
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-dotnettools.vscode-dotnet-runtime
+code --install-extension ms-python.black-formatter
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension ms-vscode.cpptools-themes
+code --install-extension pkief.material-icon-theme
+code --install-extension twxs.cmake
+code --install-extension usernamehw.errorlens
+code --install-extension vscodevim.vim
